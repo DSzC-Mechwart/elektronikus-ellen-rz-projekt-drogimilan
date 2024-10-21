0cdf1312d0ccd82630da3e580a6f25f2992d1941
Bogiii11 <90392523+Bogiii11@users.noreply.github.com>
2024-10-21 17:18:58 +0200

    TantargyFelvitel végleges

---
+++ TantargyFelvitel/TantargyFelvitel/AdminWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace TantargyFelvitel
+{
+    public partial class AdminWindow : Window
+    {
+        private const string FilePath = "tantargyak.txt";
+
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Kimutatas_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(FilePath))
+            {
+                MessageBox.Show("Nincsenek tantárgyak az állományban.");
+                return;
+            }
+
+            var tantargyak = File.ReadAllLines(FilePath);
+            var kimutatas = new Dictionary<(int evfolyam, string tipus), (int szam, int evesOraszam)>();
+
+            foreach (var tantargy in tantargyak)
+            {
+                var reszletek = tantargy.Split(new[] { ", " }, StringSplitOptions.None);
+                if (reszletek.Length >= 5)
+                {
+                    int evfolyam = int.Parse(reszletek[1].Split(':')[1].Trim());
+                    string tipus = reszletek[2].Split(':')[1].Trim();
+                    int evesOraszam = int.Parse(reszletek[4].Split(':')[1].Trim());
+
+                    var key = (evfolyam, tipus);
+                    if (!kimutatas.ContainsKey(key))
+                    {
+                        kimutatas[key] = (0, 0);
+                    }
+                    kimutatas[key] = (kimutatas[key].szam + 1, kimutatas[key].evesOraszam + evesOraszam);
+                }
+            }
+
+            KimutatasListBox.Items.Clear();
+            foreach (var item in kimutatas)
+            {
+                KimutatasListBox.Items.Add($"Évfolyam: {item.Key.evfolyam}, Típus: {item.Key.tipus}, Tantárgyak száma: {item.Value.szam}, Összesített óraszám: {item.Value.evesOraszam}");
+            }
+        }
+    }
+}
---
+++ TantargyFelvitel/TantargyFelvitel/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TantargyFelvitel
+{
+    public partial class MainWindow : Window
+    {
+        private const string FilePath = "tantargyak.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadTantargyak();
+        }
+
+        private void LoadTantargyak()
+        {
+            if (File.Exists(FilePath))
+            {
+                var tantargyak = File.ReadAllLines(FilePath);
+                foreach (var tantargy in tantargyak)
+                {
+                    TantargyListBox.Items.Add(tantargy);
+                }
+            }
+        }
+
+        private void Mentés_Click(object sender, RoutedEventArgs e)
+        {
+            string tantargyNev = TantargyTextBox.Text;
+            int evfolyam = int.Parse(((ComboBoxItem)EvfolyamComboBox.SelectedItem).Content.ToString());
+            string tipus = ((ComboBoxItem)TipusComboBox.SelectedItem).Content.ToString();
+            int hetiOraszam = int.Parse(HetiOraszamTextBox.Text);
+            int evesOraszam = SzamitasEvesOraszam(evfolyam, tipus, hetiOraszam);
+
+            string tantargyInfo = $"{tantargyNev}, Évfolyam: {evfolyam}, Típus: {tipus}, Heti óraszám: {hetiOraszam}, Éves óraszám: {evesOraszam}";
+            TantargyListBox.Items.Add(tantargyInfo);
+
+            File.AppendAllText(FilePath, tantargyInfo + Environment.NewLine);
+        }
+
+        private int SzamitasEvesOraszam(int evfolyam, string tipus, int hetiOraszam)
+        {
+            int hetSzam = 36; 
+
+            if (evfolyam == 12)
+            {
+                hetSzam = tipus == "Közismereti" ? 31 : 36;
+            }
+            else if (evfolyam == 13)
+            {
+                hetSzam = 31;
+            }
+            else if (evfolyam < 9 || evfolyam > 11)
+            {
+                throw new Exception("Érvénytelen évfolyam.");
+            }
+
+            return hetiOraszam * hetSzam;
+        }
+
+        private void Torles_Click(object sender, RoutedEventArgs e)
+        {
+            if (TantargyListBox.SelectedItem != null)
+            {
+                string selectedTantargy = TantargyListBox.SelectedItem.ToString();
+                TantargyListBox.Items.Remove(selectedTantargy);
+                SaveTantargyak();
+            }
+            else
+            {
+                MessageBox.Show("Kérlek válassz ki egy tantárgyat a törléshez.");
+            }
+        }
+
+        private void SaveTantargyak()
+        {
+            File.WriteAllLines(FilePath, TantargyListBox.Items.Cast<string>().ToArray());
+        }
+
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            var adminWindow = new AdminWindow();
+            adminWindow.Show();
+        }
+
+        private void Hozzarendel_Click(object sender, RoutedEventArgs e)
+        {
+            if (TantargyListBox.SelectedItem == null)
+            {
+                MessageBox.Show("Kérlek válassz ki egy tantárgyat.");
+                return;
+            }
+
+            string selectedTantargy = TantargyListBox.SelectedItem.ToString();
+            string selectedTanulo = ((ComboBoxItem)TanuloComboBox.SelectedItem).Content.ToString();
+
+            TanulokTantargyaiListBox.Items.Add($"{selectedTanulo} - {selectedTantargy}");
+        }
+
+    }
+}
