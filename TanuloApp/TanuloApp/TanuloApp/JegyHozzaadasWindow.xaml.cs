01405624e46ab2f5c29158353d7a2ad692e8f97c
azsdup <90392506+azsdup@users.noreply.github.com>
2024-10-21 16:05:35 +0200

    3. Végleges

---
+++ TanuloApp/TanuloApp/TanuloApp/JegyHozzaadasWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace TanuloApp
+{
+    public partial class JegyHozzaadasWindow : Window
+    {
+        private Tantargy kiválasztottTantargy;
+
+        public JegyHozzaadasWindow(Tantargy tantargy)
+        {
+            InitializeComponent();
+            kiválasztottTantargy = tantargy;
+        }
+
+        private void Hozzaad_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int ertek;
+            if (int.TryParse(ertekTextBox.Text, out ertek))
+            {
+                string tema = temaTextBox.Text;
+                string tipus = tipusTextBox.Text;
+
+                Jegy ujJegy = new Jegy
+                {
+                    Ertek = ertek,
+                    Tema = tema,
+                    Tipus = tipus,
+                    Datum = System.DateTime.Now
+                };
+
+                kiválasztottTantargy.Jegyek.Add(ujJegy);
+                this.DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Érvénytelen érték!");
+            }
+        }
+    }
+}
