c02d995e5960f78765371248214a1d6208e02dd5
unknown <dorogimilan58@gmail.com>
2024-10-23 00:47:47 +0200

    https://github.com/DSzC-Mechwart/elektronikus-ellen-rz-projekt-drogimilan.git

---
+++ Projekt 1/Projekt 1/AdminWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt_1
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        private StudentRepository studentRepository;
+
+        public AdminWindow(StudentRepository repository)
+        {
+            InitializeComponent();
+            studentRepository = repository;
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            var students = studentRepository.GetAllStudents();
+            StudentsDataGrid.ItemsSource = students;
+            UpdateStatistics(students);
+        }
+
+        private void UpdateStatistics(List<Tanulo> students)
+        {
+            int dormitoryCount = studentRepository.CountDormitoryStudents();
+            int debrecenCount = studentRepository.CountStudentsFromDebrecen();
+            int commutingCount = studentRepository.CountCommutingStudents();
+
+            DormitoryCountTextBlock.Text = $"Kollégista: {dormitoryCount}";
+            DebrecenCountTextBlock.Text = $"Debreceni: {debrecenCount}";
+            CommutingCountTextBlock.Text = $"Bejárós: {commutingCount}";
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedStudent = (Tanulo)StudentsDataGrid.SelectedItem;
+            if (selectedStudent != null)
+            {
+                var result = MessageBox.Show($"Biztosan törli {selectedStudent.Name} adatait?", "Törlés megerősítése", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    studentRepository.DeleteStudent(selectedStudent);
+                    LoadStudents();
+                }
+            }
+        }
+    }
+}
---
+++ Projekt 1/Projekt 1/StudentRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Projekt_1
+{
+    public class StudentRepository
+    {
+        private const string FilePath = "students.txt";
+        private List<Tanulo> students;
+
+        public StudentRepository()
+        {
+            students = LoadStudents();
+        }
+
+        private List<Tanulo> LoadStudents()
+        {
+            if (!File.Exists(FilePath))
+                return new List<Tanulo>();
+
+            var lines = File.ReadAllLines(FilePath);
+            return lines.Select(line =>
+            {
+                var parts = line.Split(';');
+                return new Tanulo
+                {
+                    Name = parts[0],
+                    Birthplace = parts[1],
+                    BirthDate = DateTime.Parse(parts[2]),
+                    MotherName = parts[3],
+                    Address = parts[4],
+                    EnrollmentDate = DateTime.Parse(parts[5]),
+                    Major = parts[6],
+                    Class = parts[7],
+                    IsDormitory = bool.Parse(parts[8]),
+                    Dormitory = parts[9],
+                    LogNumber = int.Parse(parts[10]),
+                    RegisterNumber = parts[11]
+                };
+            }).ToList();
+        }
+
+        public void SaveStudent(Tanulo student)
+        {
+            students.Add(student);
+            File.AppendAllText(FilePath, student.ToString() + Environment.NewLine);
+        }
+
+        public List<Tanulo> GetAllStudents()
+        {
+            return students;
+        }
+
+        public void DeleteStudent(Tanulo student)
+        {
+            students.Remove(student);
+            File.WriteAllLines(FilePath, students.Select(s => s.ToString()));
+        }
+
+        public int CountDormitoryStudents() => students.Count(s => s.IsDormitory);
+        public int CountStudentsFromDebrecen() => students.Count(s => s.Birthplace.ToLower() == "debrecen");
+        public int CountCommutingStudents() => students.Count(s => !s.IsDormitory);
+    }
+}
